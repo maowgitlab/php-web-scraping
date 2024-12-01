1dc2052b9fa9d39bacf20ad63d2f080492d4bafb
maowgit-lab <robianoor2002@gmail.com>
2024-12-01 17:09:05 +0800

    Update level-1.php

---
+++ level-1.php
@@ -0,0 +1,19 @@
+<?php
+$ch = curl_init();
+
+$url = "TODO";
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+$response = curl_exec($ch);
+
+if(curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+} else {
+    echo $response;
+}
+
+curl_close($ch);
+?>
